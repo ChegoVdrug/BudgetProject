3555dd71bbfda4376b68fb01d403ed3f76eb110c
Svetlana Mazur <sveta_zolot@mail.ru>
2020-04-12 19:35:43 +0300

    through while

--- lesson2/script.js
+++ lesson2/script.js
@@ -1,8 +1,7 @@
 'use strict';
 
-var money = +prompt("Ваш бюджет на месяц", "9990");
-var time = prompt("Введите дату в формате YYYY-MM-DD", "2020-12-05");
-
+var money = prompt("Ваш бюджет на месяц", "9990");
+var time = +prompt("Введите дату в формате YYYY-MM-DD", "2020-12-05");
 var appData = {
     budget: money,
     timeData: time,
@@ -11,15 +10,46 @@ var appData = {
     income: [],
     savings: false
 };
-
-for(let i = 0; i < 2; i++){
+// for (let i = 1; i <= 2; i++) {
+//     var state = prompt("Введите обязательную статью расходов в этом месяце", "something"+i);
+//     var sum = prompt("Во сколько обойдется?", "5");
+//     if ((typeof (state) === 'string') && (typeof (sum) != null) &&
+//         (typeof (sum) != null) && (sum != '') && (state != '') && (state.length != 50)) {
+//         appData.expenses[state] = sum;
+//     }
+//     else{
+//         alert("Input data is invalid");
+//     }
+// }
+while ((Object.keys(appData.expenses)).length < 2) {
+    console.log('--------------------');
+    console.log((Object.keys(appData.expenses)).length);
+    console.log('--------------------');
     var state = prompt("Введите обязательную статью расходов в этом месяце", "something");
     var sum = prompt("Во сколько обойдется?", "5");
-    if((typeof(state)==='string') && (typeof(sum)!=null) && 
-    (typeof(sum)!=null) && (sum!='') && (state!='') && (state.length != 50)){
+    if ((typeof (state) === 'string') && (typeof (sum) != null) &&
+        (typeof (sum) != null) && (sum != '') && (state != '') && (state.length != 50)) {
         appData.expenses[state] = sum;
-} 
+    }
+    else{
+        alert("Input data is invalid");
+    }
 }
 
+// do {
+//   //  console.log((Object.keys(appData.expenses)).length);
+//     var state = prompt("Введите обязательную статью расходов в этом месяце", "something");
+//     var sum = prompt("Во сколько обойдется?", "5");
+//     if ((typeof (state) === 'string') && (typeof (sum) != null) &&
+//         (typeof (sum) != null) && (sum != '') && (state != '') && (state.length != 50)) {
+//         appData.expenses[state] = sum;
+//     }
+//     else{
+//         alert("Input data is invalid");
+//     }
+// }
+// while ((Object.keys(appData.expenses)).length < 2);
 
-alert(appData.budget/30);
+console.log((Object.keys(appData.expenses)).length);
+console.log(Object.keys(appData.expenses));
+alert(appData.budget / 30);

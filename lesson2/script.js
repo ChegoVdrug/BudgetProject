b966f751c592fa9e2e95d1c2e36ab1212eaa01f5
Svetlana Mazur <sveta_zolot@mail.ru>
2020-04-12 19:38:30 +0300

    through for

--- lesson2/script.js
+++ lesson2/script.js
@@ -10,17 +10,17 @@ var appData = {
     income: [],
     savings: false
 };
-// for (let i = 1; i <= 2; i++) {
-//     var state = prompt("Введите обязательную статью расходов в этом месяце", "something"+i);
-//     var sum = prompt("Во сколько обойдется?", "5");
-//     if ((typeof (state) === 'string') && (typeof (sum) != null) &&
-//         (typeof (sum) != null) && (sum != '') && (state != '') && (state.length != 50)) {
-//         appData.expenses[state] = sum;
-//     }
-//     else{
-//         alert("Input data is invalid");
-//     }
-// }
+for (let i = 1; i <= 2; i++) {
+    var state = prompt("Введите обязательную статью расходов в этом месяце", "something"+i);
+    var sum = prompt("Во сколько обойдется?", "5");
+    if ((typeof (state) === 'string') && (typeof (sum) != null) &&
+        (typeof (sum) != null) && (sum != '') && (state != '') && (state.length != 50)) {
+        appData.expenses[state] = sum;
+    }
+    else{
+        alert("Input data is invalid");
+    }
+}
 // while ((Object.keys(appData.expenses)).length < 2) {
 //     console.log('--------------------');
 //     console.log((Object.keys(appData.expenses)).length);
@@ -36,19 +36,19 @@ var appData = {
 //     }
 // }
 
-do {
-  //  console.log((Object.keys(appData.expenses)).length);
-    var state = prompt("Введите обязательную статью расходов в этом месяце", "something");
-    var sum = prompt("Во сколько обойдется?", "5");
-    if ((typeof (state) === 'string') && (typeof (sum) != null) &&
-        (typeof (sum) != null) && (sum != '') && (state != '') && (state.length != 50)) {
-        appData.expenses[state] = sum;
-    }
-    else{
-        alert("Input data is invalid");
-    }
-}
-while ((Object.keys(appData.expenses)).length < 2);
+// do {
+//   //  console.log((Object.keys(appData.expenses)).length);
+//     var state = prompt("Введите обязательную статью расходов в этом месяце", "something");
+//     var sum = prompt("Во сколько обойдется?", "5");
+//     if ((typeof (state) === 'string') && (typeof (sum) != null) &&
+//         (typeof (sum) != null) && (sum != '') && (state != '') && (state.length != 50)) {
+//         appData.expenses[state] = sum;
+//     }
+//     else{
+//         alert("Input data is invalid");
+//     }
+// }
+// while ((Object.keys(appData.expenses)).length < 2);
 
 console.log((Object.keys(appData.expenses)).length);
 console.log(Object.keys(appData.expenses));

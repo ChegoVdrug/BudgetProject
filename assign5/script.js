099dd4e5a4e635846962ba5778eb63e5ead23693
Svetlana Mazur <sveta_zolot@mail.ru>
2020-04-19 14:50:01 +0300

    solved assign5

---
+++ assign5/script.js
@@ -0,0 +1,37 @@
+// let x = 5; 
+// alert( x++ );
+// alert(x);
+
+//console.log( [ ] + false - null + true );
+
+// let y = 1;
+// let x = y = 2; 
+// console.log(x);
+
+// let arr = [1,2,2,9];
+// console.log(arr);
+// console.log(arr + 1 + 2);
+
+// console.log("1"[0]);
+
+
+ //if(2 && 1 && null && 0 && undefined){
+//     console.log("1");  
+// }
+// else console.log("0"); 
+
+
+alert( null || 2 && 4 || 3 );
+console.log( 2 && 1 && null && 0 && undefined);
+
+let a, b;
+a = [1, 2, 3]; 
+b = [1, 2, 3]; 
+b = a;
+b = [1, 2, 3]; 
+console.log(a == b);
+
+console.log( typeof(+"Infinity" ));
+
+console.log( "ёжик" > "яблоко");
+console.log( 0 || "" || 2 || undefined || true || falsе);

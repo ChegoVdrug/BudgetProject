a89fc4f3d08b17c7b7331cd5ff6f5829f6f02368
Svetlana Mazur <sveta_zolot@mail.ru>
2020-04-20 14:38:37 +0300

    correct solution

--- assign6/js/script.js
+++ assign6/js/script.js
@@ -1,8 +1,8 @@
 let menu = document.querySelector('.menu');
 let liList = document.getElementsByClassName('menu-item');
-let title = document.getElementsByClassName('title');
-let adv = document.getElementsByClassName('adv');
-let textBox = document.getElementsByClassName('prompt');
+let title = document.getElementsByClassName('title')[0];
+let adv = document.getElementsByClassName('adv')[0];
+let textBox = document.querySelector('#prompt');
 //create 5th li
 let li = document.createElement('li');
 li.classList.add('menu-item');
@@ -10,18 +10,21 @@ li.innerHTML='Пятый пункт';
 menu.appendChild(li);
 
 //insert li3 before li2
-liList[2].replaceWith(liList[1]);
+menu.insertBefore(liList[2],liList[1]);
 liList = document.getElementsByClassName('menu-item');
 
-//create and insert new li2
-let li2 = document.createElement('li');
-li2.classList.add('menu-item');
-li2.innerHTML = ('Второй пункт');
-menu.insertBefore(li2, liList[1]);
+//change background image
+document.body.style.backgroundImage = "url(../img/apple_true.jpg)";
+
+// //create and insert new li2
+// let li2 = document.createElement('li');
+// li2.classList.add('menu-item');
+// li2.innerHTML = ('Второй пункт');
+// menu.insertBefore(li2, liList[1]);
 //change the text content
-title[0].innerHTML = "Мы продаем только подлинную технику Apple";
+title.innerHTML = "Мы продаем только подлинную технику Apple";
 //delete an advertisement
-adv[0].remove();
+adv.remove();
 //get info from user and insert it to div
 let result = prompt("Do you like Apple?", '');
-textBox[0].innerHTML = result;
+textBox.innerHTML = result;
